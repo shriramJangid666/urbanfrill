f902d7607e2de0235ae26e8d51884ec166e259ff
shriramJangid666 <jangidshriram09@gmail.com>
2025-11-09 01:03:18 +0530

    tyring something

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/urbanfrill/',
   plugins: [react()],
-  base: '/urbanfrill/', // <-- important for GitHub Pages
 })
+
